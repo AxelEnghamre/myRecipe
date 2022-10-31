3ff0acfb589c7c3e9593239530411233cc668fba
Axel Enghamre <axel@enghamre.se>
2022-10-31 16:36:46 +0100

    tilte to login

--- login/index.php
+++ login/index.php
@@ -12,7 +12,7 @@ require_once(__DIR__ . "./../src/components/menu.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/dist/output.css">
-    <title>myRecipe</title>
+    <title>login</title>
 </head>
 
 <body>
