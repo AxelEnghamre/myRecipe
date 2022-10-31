5dcc95607ce73bf9ec1812d54965e5bfae5d2d5e
Axel Enghamre <axel@enghamre.se>
2022-10-31 14:48:38 +0100

    Added env sample which needs to be copied into env.php

---
+++ env.sample.php
@@ -0,0 +1,10 @@
+<?php
+// copy this file to env.php
+// setup config vars for enviroment
+
+
+// database config
+putenv('db-host=');
+putenv('db-user=root');
+putenv('db-pwd=');
+putenv('db-name=');
