c13a8a74821181769bb70a229d869523bb074035
Axel Enghamre <axel@enghamre.se>
2022-10-31 17:35:31 +0100

    Added basic user class

---
+++ src/classes/database/users.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+require_once(__DIR__ . "/dataBase.php");
+
+class users extends dataBase
+{
+    // get all user properties from id
+    public function getUser(int $id): array
+    {
+        $sql = "SELECT * FROM users WHERE id = ?";
+        $query = $this->connect()->prepare($sql);
+        $query->execute([
+            $id
+        ]);
+
+        $response = $query->fetch();
+
+        return $response;
+    }
+
+    // get all user properties from name and pwd
+    public function getUserFromNameAndPwd(string $name, string $pwd): array
+    {
+        $sql = "SELECT * FROM users WHERE name = ? AND pwd = ?";
+        $query = $this->connect()->prepare($sql);
+        $query->execute([
+            $name,
+            $pwd
+        ]);
+
+        $response = $query->fetch();
+
+        return $response;
+    }
+}
