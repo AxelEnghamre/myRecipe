8be2595a1ed2f9c4395cbe731de0ee61af0d7f92
Axel Enghamre <axel@enghamre.se>
2022-10-31 17:07:02 +0100

    Added login form and addded the main app class

--- login/index.php
+++ login/index.php
@@ -1,5 +1,14 @@
 <?php
+require_once(__DIR__ . "./../src/classes/app.php");
 require_once(__DIR__ . "./../src/components/menu.php");
+
+$app = new app;
+
+// if the user is signed in then redirect to dashboard
+if ($app->getIsSignedIn()) {
+    header("Location: ../dashboard");
+    exit;
+}
 
 ?>
 
@@ -15,8 +24,21 @@ require_once(__DIR__ . "./../src/components/menu.php");
     <title>login</title>
 </head>
 
-<body class="p-2 pt-10 bg-lemon-milk">
+<body class="bg-lemon-milk p-2 gap-10 grid place-items-center w-screen min-h-screen">
     <?php menu() ?>
+
+    <form action="api/login.php" method="POST" class="flex flex-col gap-2 p-2 bg-cream rounded-xl">
+        <ul class="w-full flex flex-col items-center text-warning">
+            <?php
+            foreach ($app->getErrors() as $error) {
+                echo "<li>$error</li>";
+            }
+            ?>
+        </ul>
+        <input type="text" name="user" id="user" placeholder="Username" class="bg-cream placeholder:text-coffee">
+        <input type="password" name="pwd" id="pwd" placeholder="Password" class="bg-cream placeholder:text-coffee">
+        <input type="submit" value="login" class="w-full bg-coffee text-cream rounded-lg hover:cursor-pointer">
+    </form>
 </body>
 
 </html>
