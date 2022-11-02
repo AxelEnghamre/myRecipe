d2fe743fa38f649b086a2badcb72806f5f86ca63
Axel Enghamre <axel@enghamre.se>
2022-11-02 21:00:05 +0100

    styling

--- dashboard/index.php
+++ dashboard/index.php
@@ -38,6 +38,11 @@ $userRecipes = array_map(function ($id) use ($recipes) {
     <?php menu($app->getIsSignedIn()) ?>
 
     <section>
+        <h1 class="text-4xl">Dashboard</h1>
+        <h3>Welcome <?= $app->getUserFirstName() ?> <?= $app->getUserLastName() ?></h3>
+    </section>
+
+    <section>
         <ul>
             <?php
             foreach ($app->getErrors() as $error) {
@@ -51,49 +56,47 @@ $userRecipes = array_map(function ($id) use ($recipes) {
         </ul>
     </section>
 
-    <section>
-        <h1 class="text-4xl">Dashboard</h1>
-        <h3>Welcome <?= $app->getUserFirstName() ?> <?= $app->getUserLastName() ?></h3>
-    </section>
+    <main class=" max-w-2xl ml-auto mr-auto flex flex-col gap-10">
 
-    <section>
-        <h2 class="text-2xl">Your Recipes</h2>
-        <ul>
-            <?php
-            foreach ($userRecipes as $recipe) {
-            ?>
-                <li class="w-full flex flex-row gap-4 content-center">
-                    <a href="/edit/?recipe_id=<?= $recipe['id'] ?>" class="w-14 h-8 bg-cream rounded-xl grid place-items-center">edit</a>
-                    <a href="/api/recipe/delete.php?recipe_id=<?= $recipe['id'] ?>" class="w-14 h-8 bg-warning rounded-xl grid place-items-center">delete</a>
-                    <p class="grid place-content-center"><?= $recipe['name'] ?></p>
-                </li>
-            <?php
-            }
-            ?>
-        </ul>
-    </section>
+        <section class="flex flex-col gap-6">
+            <h2 class="text-2xl">Your Recipes</h2>
+            <ul class="flex flex-col gap-5">
+                <?php
+                foreach ($userRecipes as $recipe) {
+                ?>
+                    <li class="w-full flex flex-row gap-4 content-center p-2 bg-coffee rounded-xl text-cream">
+                        <a href="/edit/?recipe_id=<?= $recipe['id'] ?>" class="w-14 h-8 bg-cream rounded-xl grid place-items-center text-coffee">edit</a>
+                        <a href="/api/recipe/delete.php?recipe_id=<?= $recipe['id'] ?>" class="w-14 h-8 bg-warning rounded-xl grid place-items-center text-coffee">delete</a>
+                        <p class="grid place-content-center"><?= $recipe['name'] ?></p>
+                    </li>
+                <?php
+                }
+                ?>
+            </ul>
+        </section>
 
-    <section>
-        <h2 class="text-2xl">Create a Recipe</h2>
-        <form action="/api/recipe/create.php" method="POST" class="w-full flex flex-col gap-4">
-            <section class="w-full flex flex-col gap-1">
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name" placeholder="Recipe name" required>
-            </section>
+        <section class="flex flex-col gap-6">
+            <h2 class="text-2xl">Create a Recipe</h2>
+            <form action="/api/recipe/create.php" method="POST" class="w-full flex flex-col gap-4 bg-coffee p-4 rounded-xl text-cream">
+                <section class="w-full flex flex-col gap-1">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Recipe name" class="bg-coffee rounded-lg focus:bg-cream duration-100 border-coffee focus:border-cream border-2 p-1 text-cream focus:text-coffee placeholder:text-warning" required>
+                </section>
 
-            <section class="w-full flex flex-col gap-1">
-                <label for="shortDescription">Short description</label>
-                <textarea type="textarea" name="shortDescription" id="shortDescription" placeholder="Recipe short description" required></textarea>
-            </section>
+                <section class="w-full flex flex-col gap-1">
+                    <label for="shortDescription">Short description</label>
+                    <textarea type="textarea" name="shortDescription" id="shortDescription" placeholder="Recipe short description" class="bg-coffee rounded-lg focus:bg-cream duration-100 border-coffee focus:border-cream border-2 p-1 text-cream focus:text-coffee placeholder:text-warning" required></textarea>
+                </section>
 
-            <section class="w-full flex flex-col gap-1">
-                <label for="description">Description</label>
-                <textarea type="textarea" name="description" id="description" placeholder="Recipe description" required></textarea>
-            </section>
+                <section class="w-full flex flex-col gap-1">
+                    <label for="description">Description</label>
+                    <textarea type="textarea" name="description" id="description" placeholder="Recipe description" class="bg-coffee rounded-lg focus:bg-cream duration-100 border-coffee focus:border-cream border-2 p-1 text-cream focus:text-coffee placeholder:text-warning" required></textarea>
+                </section>
 
-            <input type="submit" value="Add Recipe" class="w-32 h-8 bg-cream rounded-xl grid place-items-center hover:cursor-pointer">
-        </form>
-    </section>
+                <input type="submit" value="Add Recipe" class="w-32 h-8 bg-cream text-coffee rounded-xl grid place-items-center hover:cursor-pointer">
+            </form>
+        </section>
+    </main>
 
 </body>
 
