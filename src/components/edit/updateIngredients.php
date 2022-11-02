a26778f8e8435328a5894e3e6d1fb34733cbcf67
Axel Enghamre <axel@enghamre.se>
2022-11-02 19:23:42 +0100

    basic  styling

--- src/components/edit/updateIngredients.php
+++ src/components/edit/updateIngredients.php
@@ -6,26 +6,34 @@ declare(strict_types=1);
 function updateIngredients(array $ingredientsArray): void
 {
 ?>
-    <ul>
+    <ul class="w-full flex flex-col gap-3">
         <?php
         foreach ($ingredientsArray as $ingredient) {
         ?>
-            <li>
-                <form action="/api/ingredient/update.php" method="post">
+            <li class="w-full">
+                <form action="/api/ingredient/update.php" method="post" class="w-full flex flex-col gap-4 bg-coffee p-4 rounded-xl text-cream overflow-hidden">
                     <input type="hidden" name="ingredientId" value=<?= $ingredient['id'] ?>>
 
-                    <label for="ingredient">ingredient</label>
-                    <input type="text" name="ingredient" value="<?= $ingredient['ingredient'] ?>">
+                    <section class="flex flex-row ">
+                        <label for="ingredient" class=" w-24 flex items-center justify-end">ingredient</label>
+                        <input type="text" name="ingredient" class="flex-1 bg-coffee rounded-lg focus:bg-cream duration-100 border-coffee focus:border-cream border-2 p-1 text-cream focus:text-coffee" value="<?= $ingredient['ingredient'] ?>">
+                    </section>
 
-                    <label for="amount">amount</label>
-                    <input type="number" name="amount" value="<?= $ingredient['amount'] ?>">
+                    <section class="flex flex-row ">
+                        <label for="amount" class=" w-24 flex items-center justify-end">amount</label>
+                        <input type="number" name="amount" class="flex-1 bg-coffee rounded-lg focus:bg-cream duration-100 border-coffee focus:border-cream border-2 p-1 text-cream focus:text-coffee" value="<?= $ingredient['amount'] ?>">
+                    </section>
 
-                    <label for="unit">unit</label>
-                    <input type="text" name="unit" value="<?= $ingredient['unit'] ?>">
+                    <section class="flex flex-row ">
+                        <label for="unit" class=" w-24 flex items-center justify-end">unit</label>
+                        <input type="text" name="unit" class="flex-1 bg-coffee rounded-lg focus:bg-cream duration-100 border-coffee focus:border-cream border-2 p-1 text-cream focus:text-coffee" value="<?= $ingredient['unit'] ?>">
+                    </section>
 
-                    <input type="submit" value="save">
+                    <section class="flex flex-row gap-6">
+                        <input type="submit" value="save" class="w-14 h-8 bg-cream text-coffee rounded-xl grid place-items-center hover:cursor-pointer">
+                        <a href="/api/ingredient/delete.php?ingredientId=<?= $ingredient['id'] ?>" class="w-14 h-8 bg-warning text-coffee rounded-xl grid place-items-center hover:cursor-pointer">delete</a>
+                    </section>
 
-                    <a href="/api/ingredient/delete.php?ingredientId=<?= $ingredient['id'] ?>">delete</a>
                 </form>
             </li>
         <?php
